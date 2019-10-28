aeadcf3f67e54ab36977565fa59f5189732a1562
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-10-27 22:14:45 -0700

    Added welcome discount offer

--- script.js
+++ script.js
@@ -29,6 +29,20 @@ $(function() {
   setInterval(() => {
     $('#next').click();
   }, 5000);
+
+  // Event listener to open welcome offer
+  $('#offer').on('click', () => {
+    $('#offer').fadeOut('slideDown', () => {
+      $('#form').fadeIn('slow');
+    });
+  });
+
+  // Event listener to close welcome offer
+  $('.close').on('click', () => {
+    $('#form').fadeOut('slow', () => {
+      $('#offer').fadeIn('slow');
+    });
+  });
 });
 
 
