c55e22c50ef83521a44dc34b820178877d6d3b44
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-10-28 23:02:56 -0700

    Used bootstrap modal for welcome discount offer

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 $(function() {
-  // Event listener to open welcome offer
-  $('#offer').on('click', () => {
-    $('#offer').fadeOut('slideDown', () => {
-      $('#form').fadeIn('slow');
-    });
-  });
 
-  // Event listener to close welcome offer
-  $('.close').on('click', () => {
-    $('#form').fadeOut('slow', () => {
-      $('#offer').fadeIn('slow');
-    });
-  });
 });
