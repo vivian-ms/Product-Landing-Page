edb368df5290f5ba130fb7f8d07d273e1677a489
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2019-10-29 22:24:03 -0700

    Made navbar collapse after clicking on nav items. Made dropdown items into carousel indicators. Synced carousel and tab-content w/ each other

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(function() {
+  $('#nav-bar .nav-item:not(.dropdown)').on('click', function(evt) {
+    // Collapse navbar after clicking nav item (except the one for dropdown menu)
+    $('.navbar-collapse').collapse('hide');
+  });
 
+  $('.dropdown-item').on('click', function(evt) {
+    // Collapse navbar after click
+    $('.navbar-collapse').collapse('hide');
+
+    // Use dropdown items as indicators for carousel
+    let index = $('.dropdown-item').index($(this));
+    $('#product_slides').carousel(index);
+  });
+
+  // Prevent search form from submitting
+  $('.form inline').on('submit', function(evt) {
+    evt.preventDefault();
+  });
+
+  // Sync tab-content with carousel
+  $('#product_slides').on('slid.bs.carousel', function(evt) {
+    let tabs = $('.nav-tabs a');
+    $(tabs[evt.to]).tab('show');
+  });
+
+  // Use nav-tabs as indicators for carousel
+  $('.nav-tabs a').on('click', function(evt) {
+    let tabs = $('.nav-tabs a');
+    let index = tabs.index($(this));
+    $('#product_slides').carousel(index);
+  });
 });
